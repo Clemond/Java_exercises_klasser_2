868e80cf2826a46883c996a5aaf177db1fabfd1c
Nicholas Nieminen Jönsson <nicholasnieminenjonsson@MacBook-Pro-som-tillhor-Nicholas.local>
2023-10-04 11:27:09 +0200

    Excercise 3

--- Student.java
+++ Student.java
@@ -1,14 +1,21 @@
 public class Student {
 
-    String forname;
-    String lastname;
-    String nameOfSchool;
-    int age;
+    private String forname;
+    private String lastname;
+    private String nameOfSchool;
+    private int age;
 
+    //Konstruktor! //setter
+    public Student(String forname, String lastname, String nameOfSchool, int age){
+        this.forname = forname;
+        this.lastname = lastname;
+        this.nameOfSchool = nameOfSchool;
+        this.age = age;
+    }
 
+    //getter
     public String getInfo() {
-        return "Student: " + forname + " " + lastname + ". " + "School: " 
-                           + nameOfSchool + ". " + "Age: " +  age;
+        return "Student: " + forname + " " + lastname + ". " + "School: " + nameOfSchool + ". " + "Age: " +  age;
     }
     
 }
